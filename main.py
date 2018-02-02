3e185759f2698867e12ba871e3efc523852f6992
thagen15 <thagen@wpi.edu>
2018-02-02 16:24:41 -0500

    human testing?

--- main.py
+++ main.py
@@ -4,36 +4,46 @@ if __name__ == '__main__':
 
     #White is 1, Black is 2
     color = 1
-    opponent = 0
+    # opponent = 0
     #Have some randomizer to determine order of players
-
+    moves=0
     #While the end of game file isn't there
     while not os.path.isfile('endgame.txt'): #main python file to run our code from
 
 
         #is our prompt file in the path
         #ensure we are not writing and checking to fast
-        time.sleep(.1)
-        if os.path.isfile('GROUPNAME.go'): #main python file to run our code from
+        # time.sleep(.1)
+        # if os.path.isfile('AIWithFriends.go'): #main python file to run our code from
+        if moves %2 ==1:
+            column=raw_input("Column: ")
+            row=raw_input("Row: ")
+            moves++
+            file = open("move_file.txt", "w")
+            file.write('Human'+ str(column) + str(row))
+            file.close()
 
+        if moves %2 ==0:
             #read the move.txt file and update the board
             file = open("move_file.txt", "r")
             move = str(file.read())
             file.close()
             #if the file is empty, this is the first move of the game so we are the white stones
-            if move == []:
-                color = 0
-                opponent = 1
+            # if move == []:
+            #     color = 0
+            #     opponent = 1
 
             #Otherwise, a move has been made. Process the latest move and update our board
-            else:
-                #move is split by spaces into 3 things, groupName Column Row
-                board.placeStone(opponent, move[1], move[2])
+            # else:
+            #move is split by spaces into 3 things, groupName Column Row
+            board.placeStone(opponent, move[1], move[2])
 
-                #Process the board and do magic things to do best move
-                optimalMove = board.minimax().getPosition()
-                board.placeStone(opponent, optimalMove[0],optimalMove[1])
-                #lastly write to the file
-                file = open("move_file.txt", "w")
-                file.write('GROUPNAME'+str(optimalMove[0])+str(optimalMove[1]))
-                file.close()
+            #Process the board and do magic things to do best move
+            optimalMove = board.minimax().getPosition()
+            print (optimalMove)
+            board.placeStone(color, optimalMove[0],optimalMove[1])
+            #lastly write to the file
+            file = open("move_file.txt", "w")
+            file.write('AIWithFriends'+str(optimalMove[0])+str(optimalMove[1]))
+            file.close()
+            moves++
